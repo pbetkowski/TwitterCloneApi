27fb09b75852506288f8abbe4607768a72c71b8c
pbetkows <przbetkowski@gmail.com>
2019-08-26 13:37:31 +0200

    Add md5 hash generator
    Users passwords will be stored in database and hashed with MD5 algorithm.

---
+++ src/main/java/com/example/TwitterClone/utils/HashService.groovy
@@ -0,0 +1,13 @@
+package com.example.TwitterClone.utils
+
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
+class HashService {
+
+    static byte [] md5Encryption(String text) {
+        byte [] bytesOfMessage = text.getBytes(StandardCharsets.UTF_8)
+        MessageDigest algorithm = MessageDigest.getInstance("MD5")
+        algorithm.digest(bytesOfMessage)
+    }
+}
